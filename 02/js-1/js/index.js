cd5ce2068f99527f9b03da623c67418e94e7df74
Christina <sweetflute@gmail.com>
2021-08-25 16:17:42 -0400

    js-1 top half exercise

--- 02/js-1/js/index.js
+++ 02/js-1/js/index.js
@@ -2,6 +2,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	//We will work on the first half of the exercise in the class here
 	//Your exercise start here
 
+	document.getElementById('menu1').addEventListener('click', function(event){
+		alert(document.getElementById('paragraph1').textContent);
+	});
+
+	document.getElementById('menu2').addEventListener('click', function(event){
+		alert(document.getElementById('paragraph2').innerHTML);
+	});
+
+	document.getElementById('menu3').addEventListener('click', function(event){
+		alert(document.getElementById('textfield1').value);
+	});
+
+	document.getElementById('menu4').addEventListener('click', function(event){
+		alert(document.getElementById('url1').getAttribute("href"));
+	});
+
+	document.getElementById('menu5').addEventListener('click', function(event){
+		alert(document.getElementById('img1').getAttribute("src"));
+	});
 	// document.getElementById("test-menu1").addEventListener("click", function(){
 	// 	alert(document.getElementById("test").textContent);
 	// });
