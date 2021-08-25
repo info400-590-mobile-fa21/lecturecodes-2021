47f907685e8c82ca974b1f2096ebd0d9910a2630
Christina <sweetflute@gmail.com>
2021-08-25 14:30:12 -0400

    lecture codes 02

---
+++ 02/js-1/js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	//We will work on the first half of the exercise in the class here
+	//Your exercise start here
+
+	// document.getElementById("test-menu1").addEventListener("click", function(){
+	// 	alert(document.getElementById("test").textContent);
+	// });
+
+});
