ee7c2109e9747274984230d7d329a3605a42c374
Christina <sweetflute@gmail.com>
2021-09-22 20:14:42 -0400

    lecture codes 08

--- 08/src/App.js
+++ 08/src/App.js
@@ -3,12 +3,43 @@ import './App.css';
 
 class App extends React.Component {
 
+  state={
+    weatherObject:null
+  }
+
+
+  getWeather = async()=>{
+    let response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=4acd6f48610c97f8c4c409daa616bcb3&units=imperial")
+    let result = await response.json()
+
+    // console.log(result.main.temp)
+    // this.setState({weatherObject:result})
+
+    return result
+  }
+
+  componentDidMount(){
+
+    // this.getWeather()
+    this.getWeather()
+    .then(result => {
+      console.log(result.main.temp)
+      this.setState({weatherObject:result})
+    })
+
+  }
+
   render(){
+
+    let temperature = 70
+    if (this.state.weatherObject)
+      temperature = this.state.weatherObject.main.temp
+
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            Current temperature: 70F
+            Current temperature: {temperature} F
           </p>
         </header>
       </div>
