cb922f5db127e25b19ab04a614a637bda24e2bb6
Christina <sweetflute@gmail.com>
2021-10-29 11:51:08 -0400

    update lecturecode 16 & 17

--- 16/App.js
+++ 16/App.js
@@ -1,20 +1,108 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Image, Dimensions} from 'react-native';
 import MapView from 'react-native-maps';// import the default export from that module
 import {Marker, Callout} from 'react-native-maps'; //import one of the export from that module
 import * as Location from 'expo-location'; //import the whole module
+import pin from './assets/pin.png'
+import classroom from './assets/classroom.png'
+import mug from './assets/cafe.png'
 
 
 export default function App() {
 
+  const [location, setLocation] = useState(null)
+
+  const getLocation = async()=>{
+    let {status} = await Location.requestForegroundPermissionsAsync()
+
+    if (status != "granted"){
+      console.log("error")
+      return
+    }
+    // let result = await Location.getCurrentPositionAsync({})
+    let result = await Location.geocodeAsync("150 S Woodlawn Ave, Bloomington, IN 47405")
+    setLocation(result)
+  }
+
+  useEffect(()=>{
+    getLocation()
+  }, [])
+
+  // let latitude  = 39.1713251
+  // let longitude = -86.6018864
+
+  // if (location){
+  //   latitude = location.coords.latitude
+  //   longitude = location.coords.longitude
+  // }
+
   return (
     <View style={styles.container}>
+    <Text>{location?location[0].latitude:null}</Text>
+    <Text>{location?location[0].longitude:null}</Text>
+    {/*<Text>{location?location.coords.latitude:null}</Text>*/}
+    {/*<Text>{location?location.coords.longitude:null}</Text>0*/}
+
+    {/*<MapView 
+    style={styles.map}
+    provider="google"
+    initialRegion={{
+      latitude:location?location.coords.latitude:39.1713251,
+      longitude:location?location.coords.longitude:39.1713251,
+      latitudeDelta:0.01,
+      longitudeDelta:0.01
+    }}/>*/}
+
     </View>
   );
 }
 
 
+/*<MapView 
+    style={styles.map}
+    provider="google"
+    initialRegion={{
+      latitude:39.1713251,
+      longitude:-86.6018864,
+      latitudeDelta:0.01,
+      longitudeDelta:0.01
+    }}>
+    
+      
+    </MapView>
+*/
+/*{cafes.map((cafe, index)=>(
+      <Marker
+        key={index}
+        coordinate={cafe.coordinate}
+        title={cafe.title}
+        description={cafe.description}
+      >
+        <Callout>
+          <View>
+            <Image source={mug} style={{width:64, height:64}}/>
+            <Text>{cafe.title}</Text>
+          </View>
+        </Callout>
+
+      </Marker>))}
+*/
+/*<Marker
+        coordinate ={{latitude:39.1653449, longitude:-86.5257769}}
+        title= "Lindley Hall"
+        description= "I400/I590 classroom"
+        // pinColor="purple"
+        image={pin}
+      >
+        <Callout>
+          <View style={{width:100, height:100}}>
+            <Text>Lindley Hall</Text>
+            <Image source={classroom} style={{width:40, height:40}}/>
+          </View>
+        </Callout>
+      </Marker>*/
+// <View style={{backgroundColor:"purple"}}><Text style={{color:"white"}}>PIN</Text></View>
 var cafes =[{
   coordinate: {latitude:39.1714804, longitude:-86.5368812},
   title:"Crumble Cafe West",
@@ -39,7 +127,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },map:{
-    width: 300,
-    height: 300
+    width: "100%",
+    height: "100%"
   }
 });
--- 17/App.js
+++ 17/App.js
@@ -6,12 +6,65 @@ import { Video } from 'expo-av';
 
 export default function App() {
 
+  const [image, setImage] = useState(null)
+  const [cameraImage, setCameraImage] = useState(null)
+
+  const openLibrary = async()=>{
+    const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if(status !== "granted"){
+      alert("Media library permission not granted")
+      return
+    }
+
+    const image = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes:ImagePicker.MediaTypeOptions.Videos,
+    })
+    setImage(image.uri)
+
+  }
+
+  const openCamera = async()=>{
+    
+    const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if(status !== "granted"){
+      alert("Media library permission is not granted")
+      return
+    }
+
+    const cameraStatus = await ImagePicker.requestCameraPermissionsAsync()
+
+    if(cameraStatus.status !== "granted"){
+      alert("Camera permission is not granted")
+      return
+    }
+
+    const image = await ImagePicker.launchCameraAsync({
+      mediaTypes:ImagePicker.MediaTypeOptions.Videos,
+      allowsEditing:true,
+      videoMaxDuration:2
+    })
+    setCameraImage(image.uri)
+
+  }
+
   return (
     <View style={styles.container}>
       <Button
         title="Select a photo"
         style={styles.button}
+        onPress={openLibrary}
       />
+      {image&&<Image source={{uri:image}} style={styles.image}/>}
+
+      <Button
+        title="Open Camera"
+        style={styles.button}
+        onPress={openCamera}
+      />
+      {/*{cameraImage&&<Image source={{uri:cameraImage}} style={styles.image}/>}*/}
+      {cameraImage&&<Video source={{uri:cameraImage}} shouldPlay={true} isLooping={true} style={styles.image}/>}
     </View>
   );
 }
