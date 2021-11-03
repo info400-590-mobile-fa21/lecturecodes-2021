f209ef171005fb0c56e58e8a2149a2ec62a53b6a
Christina <sweetflute@gmail.com>
2021-11-03 16:29:21 -0400

    update lecturecode 19

--- 19/App.js
+++ 19/App.js
@@ -7,7 +7,12 @@ import { getFirestore, setDoc, getDoc, doc } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
+  apiKey: "AIzaSyALPKf5zJUKxMsJxXKPGFysgR4eUuzIz_0",
+  authDomain: "demo19-61d58.firebaseapp.com",
+  projectId: "demo19-61d58",
+  storageBucket: "demo19-61d58.appspot.com",
+  messagingSenderId: "493958980793",
+  appId: "1:493958980793:web:aa601385cfb8d359112768"
 };
 
 // Initialize Firebase
@@ -25,9 +30,38 @@ export default function App (){
 
   const saveData = async() =>{
 
+    await setDoc(doc(firestore, "users", "1"), {
+      firstname: firstname,
+      lastname: lastname,
+      email: "abc@gmail.com"
+    });
+
   }
 
   const getData = async() =>{
+
+    // const colRef = collection(db, "users");
+
+    const docRef = doc(firestore, "users", "0ZABmxTUga8l45tb0Y9O");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+
+      let newUser = {
+        firstname: docSnap.data().firstname,
+        lastname: docSnap.data().lastname,
+        email: docSnap.data().email
+      }
+
+      setUserInfo([...userInfo, newUser])      
+
+      console.log("Document data:", docSnap.data());
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+
+
   }
 
   useEffect(()=>{
@@ -58,12 +92,12 @@ export default function App (){
           title = "delete"
           onPress = {()=>removeData(firstname)}
         />
-        {userInfo.map(user => <Users {...user}/>)}
+        {userInfo.map(user => <User {...user}/>)}
         </View>
       )
 }
 
-function Users(props){
+function User(props){
     return(
       <View>
         <Text>{props.firstname}</Text>
